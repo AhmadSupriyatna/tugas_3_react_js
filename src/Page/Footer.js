cd16abd20e86a029fa88f893a9dabf8e959d13bf
AhmadSupriyatna <ahmadsupriyatna1@gmail.com>
2021-09-19 14:01:25 +0700

    Tugas 3 React JS
    Tugas ini kami kumpulkan untuk Niomic

--- src/Page/Footer.js
+++ src/Page/Footer.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
-import TopEvents from './TopEvents'
+import TopEvents from '../Events/TopEvents'
 
 export class Footer extends Component {
     render() {
         return (
             <div>
                 <TopEvents />
-                <h3>Tugas 4 Ract JS</h3>
+                <h3>Tugas 3 Ract JS</h3>
             </div>
         );
     }
